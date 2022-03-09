b1a4df6ecf4c1e6f8cd5a2443d6f0f9f91fd06a2
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2022-03-09 04:54:02 -0500

    adding favorites functionality

--- demo/demo.js
+++ demo/demo.js
@@ -3,28 +3,30 @@ import { fetchEvents, fetchFloorPlan } from './mock-api.js'
 
 customElements.define('program-guide', ThunderEvents.Components.ProgramGuideNative)
 
-ThunderEvents.fetchGuide(async () => {
+ThunderEvents.init(async () => {
   const events = await fetchEvents()
   const imageSrc = await fetchFloorPlan()
   return {
-    events: events.map(event => ({
-      name: event.title,
-      location: event.room,
-      start: event.beginTime,
-      end: event.endTime,
-      description: event.description,
-    })),
-    floorPlan: {
-      title: 'Demo Inn',
-      imageSrc,
-      clickableAreas: [
-        {
-          shape: 'rect',
-          coords: [0, 0, 800, 689],
-          href: 'https://www.lakeside-inn.com/images/floorplans/lakeside-floorplans-all.jpg',
-          alt: 'See full map',
-        },
-      ],
+    guide: {
+      events: events.map(event => ({
+        name: event.title,
+        location: event.room,
+        start: event.beginTime,
+        end: event.endTime,
+        description: event.description,
+      })),
+      floorPlan: {
+        title: 'Demo Inn',
+        imageSrc,
+        clickableAreas: [
+          {
+            shape: 'rect',
+            coords: [0, 0, 800, 689],
+            href: 'https://www.lakeside-inn.com/images/floorplans/lakeside-floorplans-all.jpg',
+            alt: 'See full map',
+          },
+        ],
+      },
     },
   }
 })
