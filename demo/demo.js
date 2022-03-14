b43297d97ee13a7c5ff62c50ea30f16187363425
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2022-03-14 03:27:18 -0400

    changing how data is fetched for demo and adding a lot more data

--- demo/demo.js
+++ demo/demo.js
@@ -1,46 +1,19 @@
 import ThunderEvents from '../esm/thunderEvents.min.mjs'
-import { fetchEvents, fetchFloorPlan } from './mock-api.js'
 
 customElements.define('program-guide', ThunderEvents.Components.ProgramGuideNative)
 
 ThunderEvents.init(async () => {
-  const events = await fetchEvents()
-  const imageSrc = await fetchFloorPlan()
+  const response = await fetch('/getDemoEvent')
+  const data = await response.json()
   return {
+    ...data,
     guide: {
-      events: events.map(event => ({
-        name: event.title,
-        location: event.room,
-        start: event.beginTime,
-        end: event.endTime,
-        description: event.description,
-        host: event.hostedBy,
-        category: event.cat,
+      ...data.guide,
+      events: data.guide.events.map(event => ({
+        ...event,
+        start: new Date(event.start),
+        end: new Date(event.end),
       })),
-      floorPlan: {
-        title: 'Demo Inn',
-        imageSrc,
-        dimensions: {
-          height: 689,
-          width: 800,
-        },
-        clickableAreas: [
-          {
-            location: 'Main Events',
-            shape: 'rect',
-            coords: [20, 20, 227, 337],
-            href: '#View-Location-Main-Events',
-            alt: 'Main Events',
-          },
-          {
-            location: 'Panel Room',
-            shape: 'rect',
-            coords: [227, 86, 376, 288],
-            href: '#View-Location-Panel-Room',
-            alt: 'Panel Room',
-          },
-        ],
-      },
-    },
+    }
   }
 })
--- demoServer.js
+++ demoServer.js
@@ -5,5 +5,6 @@ const port = 5001
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/demo/demo.html'))
+app.get('/getDemoEvent', (req, res) => res.sendFile(__dirname + '/demoEvent.json'))
 
 app.listen(port, () => console.log(`Thunder-Events demo listening on port ${port}!`))
