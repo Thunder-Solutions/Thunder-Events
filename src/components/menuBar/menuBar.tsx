e895d6cdae1e0b95aec3c38dff989ebdc9a1f851
Jon DeWitt <jon.dewitt@thunder.solutions>
2024-07-13 16:21:19 -0400

    cleaning up the menu bar

--- src/components/menuBar/menuBar.tsx
+++ src/components/menuBar/menuBar.tsx
@@ -13,9 +13,14 @@ const Menu: Component = () => {
 				Search
 				<input />
 			</label>
-			<div class={css.dateContainer}>
-				<time datetime="2023-07-27">Thursday 7/27/2023</time>
-				<Button icon="calendar">Choose Date</Button>
+			<div class={css.dateWrapper}>
+				<div class={css.dateContainer}>
+					<time datetime="2023-07-27">
+						<span class={css.day}>Thursday</span>
+						<span class={css.date}>7/27/2023</span>
+					</time>
+					<Button icon="calendar">Choose Date</Button>
+				</div>
 			</div>
 			<Button icon="magnifier" category="menu">
 				Search
