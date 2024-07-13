6f1e1eda53f1035d0d82f196e46f7ff64c094ab8
Jon DeWitt <jon.dewitt@thunder.solutions>
2024-07-13 16:24:11 -0400

    cleaning up time header

--- src/components/timeHeader/timeHeader.tsx
+++ src/components/timeHeader/timeHeader.tsx
@@ -5,7 +5,7 @@ import Button from '../button/button';
 const TimeHeader: Component = () => {
 	return (
 		<header class={css.timeHeader}>
-			<h1>List of Events</h1>
+			<h1 class={css.timeHeading}>List of Events</h1>
 			<Button icon="clock">Choose Time</Button>
 			<Button icon="pin-magnifier">View by Location</Button>
 		</header>
